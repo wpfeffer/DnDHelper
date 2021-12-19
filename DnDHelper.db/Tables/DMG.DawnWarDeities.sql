bc953ae8121720e27cd91b688461b95ebbb65120
wpfeffer <waynee.pfeffer@gmail.com>
2021-12-19 06:50:29 -0600

    Added icon, readme, XML documentation file and SQL files

---
+++ DnDHelper.db/Tables/DMG.DawnWarDeities.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [DMG].[DawnWarDeities]
+(
+	[DawnWarDeityID] INT NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Domain] NVARCHAR(100) NOT NULL, 
+    [Alignment] NVARCHAR(2) NOT NULL, 
+    [SuggestedDomains] NVARCHAR(50) NOT NULL, 
+    [Symbol] NVARCHAR(100) NOT NULL
+)
