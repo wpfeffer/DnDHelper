9740ef1ddb1b4110318efff533ff00717c195fa1
wpfeffer <waynee.pfeffer@gmail.com>
2021-12-31 19:25:16 -0600

    Added a Forms of Government xml file and modified the associated class files.

--- DMG/Chapter1/FormOfGovernment.cs
+++ DMG/Chapter1/FormOfGovernment.cs
@@ -4,17 +4,14 @@
     {
         private readonly int _roll;
         private readonly string _government;
-        private readonly string _description;
 
         public int Roll => _roll;
         public string Government => _government;
-        public string Description => _description;
 
-        public FormOfGovernment(int roll, string government, string description)
+        public FormOfGovernment(int roll, string government)
         {
             _roll = roll;
             _government = government;
-            _description = description;
         }
     }
 }
